b719533ddd710051aee4fdffc2e8d9bfd7e3aafe
shivPosted <shivpratap621@gmail.com>
2024-06-20 19:13:31 +0530

    code dated 20.06.2024 --01 😃

---
+++ chapter-02/second.py
@@ -0,0 +1,40 @@
+# a = 34
+# name = int(input('Enter your name') #just lik js it will also return any int(input as a string)
+# # print(type(a),type(b))
+# # print('Your name is=',b)
+
+# c = 80
+
+# print('Is a greater than c', a >= c)
+# d = 9
+# z = 10
+# print('Average of c and d', (dz) / 2)
+# num = int(int(input('Enter the number')))
+# print('square of entered num is',num ** 2)
+# remainder = (c  d) % num
+# print(remainder)
+# name = 'shiv pratap'
+# print(name[0:4])
+# date = '29-06-2024'
+# # print(name, 'Good Afternoon')
+# nameNew = 'shiv  pratap'
+# print(nameNew[4:5])
+# letter  ='''
+#          Dear   '''
+# print(letter)
+
+# fruits = []
+# fruits.append(int(input('Enter student\'s marks: ')))
+# fruits.append(int(input('Enter student\'s marks: ')))
+# fruits.append(int(input('Enter student\'s marks: ')))
+# fruits.append(int(input('Enter student\'s marks: ')))
+# fruits.append(int(input('Enter student\'s marks: ')))
+# fruits.append(int(input('Enter student\'s marks: ')))
+# print(fruits)
+# fruits.sort()
+# print(fruits)
+
+# tup = ('shiv', 23, 'Unemployed') #tuple is not mutable
+# tup[1] = 21
+tup01 = (0, 45, 0, 69, 0, 12, 0)
+print(tup01.count(0))
